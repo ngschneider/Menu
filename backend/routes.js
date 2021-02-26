7cfe5ad231ae4cf98979ca476cd7fb8db4523803
Ubuntu <ubuntu@ip-172-31-7-42.us-east-2.compute.internal>
2021-02-26 02:52:29 +0000

    routes

--- backend/routes.js
+++ backend/routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const command = require("./sqlCommand");
 
-router.get("/menu", (req,res) =>	{
+router.get("/burger", (req,res) =>	{
 	console.log("received!");
-	let statement = "SELECT * FROM MenuOptions";
+statement = "SELECT * FROM burger";
 	command.send([statement], (results) => {
 		res.send(results);
 	});
--- backend/sqlCommand.js
+++ backend/sqlCommand.js
@@ -11,28 +11,8 @@ const sql = require("./events");
  * Output : sql insert command, formatted correctly
  *
  */
-	const insert = (tableName, columnSet, recordSet) => {
-		let insertString = "INSERT INTO " + tableName + " ( "; 	
-		
-		columnSet.forEach( (value, index) => {
-			insertString +=  value  + ", ";	
-		} );
-
-		insertString = insertString.substring(0, insertString.length - 2) + " ) VALUES ( ";
-		recordSet.forEach( (value,index) => {
-			insertString += "\"" + value + "\"" + ", "	
-		});
-		insertString = insertString.substring(0,insertString.length - 2) + " );"
-		
-		return insertString;
-	}
-/*
- * Input : List of sql commands
- * Output : Sends to sql database;
- */
-	let response = "";
 	const send = (sendList,cb) => {
-		let sendString = "";
+		 sendString = "";
 
 		sendList.forEach((value, index) => {
 			sendString += value + " ";
@@ -47,19 +27,4 @@ const sql = require("./events");
 		});
 	}
 
-	const select = (tableName, column, value) => {
-		let selectStatement = `SELECT * FROM ${tableName} WHERE ${column}=\'${value}\';`;
-		
-		return selectStatement;
-	
-	}
-
-	const deleteRecord = (table,column,value) => {
-		let sqlString = `DELETE FROM ${table} WHERE ${column}=\'${value}\';`;
-		return sqlString;
-	}
-
-exports.insert = insert;
 exports.send = send;
-exports.deleteRecord = deleteRecord;
-exports.select = select;
